f1b49c923b659e61812199b2b09e1846e73b5a0d
lohit123568 <87901963+lohit123568@users.noreply.github.com>
2022-02-28 17:41:15 +0530

    Add files via upload

---
+++ C81main.js
@@ -0,0 +1,28 @@
+canvas = document.getElementById("myCanvas");
+ctx= canvas.getContext("2d");
+color="red";
+Widthofline=2;
+ctx.beginPath();
+ctx.strokeStyle = color;
+ctx.lineWidth = 2;
+ctx.arc(200, 200,  40 ,0 , 2 * Math.PI);
+ctx.stroke();
+
+canvas.addEventListener("mousedown", my_mousedown);
+function my_mousedown(e){
+    color = document.getElementById("color").value;
+    radius=document.getElementById("radius").value;
+    Widthofline=document.getElementById("widthoftheline").value;
+    console.log(color);
+    mouse_x = e.clientX - canvas.offsetLeft;
+    mouse_y = e.clientY - canvas.offsetTop;
+    console.log("X = " + mouse_x + " ,Y = " + mouse_y);
+    circle(mouse_x , mouse_y);
+}
+function circle(mouse_x , mouse_y){
+    ctx.beginPath();
+ctx.strokeStyle = color;
+ctx.lineWidth = Widthofline;
+ctx.arc(mouse_x, mouse_y,  radius ,0 , 2*Math.PI);
+ctx.stroke();
+}
